f4b354ebad9cd02c248fdbb160fbda0989a3e287
srohan722 <srohan722@gmail.com>
2025-03-03 15:01:39 +0530

    Changes in calculatePoints

--- src/utils/calculatePoints.js
+++ src/utils/calculatePoints.js
@@ -12,8 +12,8 @@ export const calculatePoint = (amount) => {
   if (amount > 50 && amount <= 100) {
     return Math.floor(amount - 50);
   } else {
-    let moreThan100 = Math.floor(amount - 100) * 2;
+    return ( Math.floor(amount - 100) * 2 + 50);
 
-    return moreThan100 + 50;
+    
   }
 };
